b7f7812865a8ff43afc893b15e555d680f07f179
fahus <faduma.h@yahoo.com>
2019-11-19 10:51:52 +0000

    test created

---
+++ spec/add_listing_spec.rb
@@ -0,0 +1,14 @@
+require_relative 'add_listing'
+
+describe Listing do
+
+
+  describe '#add_listing' do
+    it 'creates a new property listing' do
+      connection = PG.connection dbname: "airbnb-test"
+
+
+end
+
+  
+end
