9573716276c37a632e999b962e614f68d1ac6aab
fahus <faduma.h@yahoo.com>
2019-11-19 12:16:38 +0000

    add listings feature passes

--- spec/setup_test_database.rb
+++ spec/setup_test_database.rb
@@ -2,4 +2,11 @@ require "db_connection"
 
 def setup_test_database
   DbConnection.setup("airbnb-test")
+  DbConnection.query("TRUNCATE listings;")
+  add_listings
+end
+
+def add_listings
+  DbConnection.setup("airbnb-test")
+  DbConnection.query("INSERT INTO listings(name, description, price) VALUES ('Hotel California', 'A lovely place to stay with lots of nice things like wifi and snacks', 1000.00)")
 end
