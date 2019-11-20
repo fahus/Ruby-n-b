20530d601103b0679a79c3a20eea1cc2e1de7c09
liatmoss <liatmoss@hotmail.com>
2019-11-20 10:12:48 +0000

    updated tests and formatting

--- spec/feature/more_detail_spec.rb
+++ spec/feature/more_detail_spec.rb
@@ -4,6 +4,8 @@ feature 'it shows specific listings in more detail' do
     visit ('/viewlistings')
     click_button 'Click here for information on Hotel California'
     expect(page).to have_content "Hotel California"
+    expect(page).to have_content "A lovely place to stay with lots of nice things like wifi and snacks"
+    expect(page).to have_content "1000.00"
   end
 
   scenario 'shows Hotel California in more detail' do
@@ -11,5 +13,7 @@ feature 'it shows specific listings in more detail' do
     visit ('/viewlistings')
     click_button 'Click here for information on Holiday Inn'
     expect(page).to have_content "Holiday Inn"
+    expect(page).to have_content "Cheap and cheerful, coffee and tea provided"
+    expect(page).to have_content "250.00"
   end
 end
