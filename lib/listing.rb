53f33b0a5508b086eeb4b833d43d268324423858
Alistair Phipps <alistair.phipps@johnlewis.co.uk>
2019-11-20 12:30:32 +0000

    owner id added to listing table and .all and .add tests passing

--- lib/listing.rb
+++ lib/listing.rb
@@ -3,13 +3,14 @@ require 'dotenv/load'
 
 class Listing
 
-  attr_reader :id, :name, :description, :price
+  attr_reader :id, :name, :description, :price, :owner_id
 
-  def initialize(id:, name:, description:, price:)
+  def initialize(id:, name:, description:, price:, owner_id:)
     @id = id
     @name = name
     @description = description
     @price = price
+    @owner_id = owner_id
   end
 
   def self.all
@@ -17,12 +18,13 @@ class Listing
     result = DbConnection.query("SELECT * FROM LISTINGS;")
     result.map do |listing|
       Listing.new(id: listing["id"], name: listing["name"],
-        description: listing["description"], price: listing["price"])
+        description: listing["description"], price: listing["price"],
+        owner_id: listing["owner_id"])
       end
     end
 
-    def self.add(name:, description:, price:)
-      result = DbConnection.query("INSERT INTO listings (name, description, price) VALUES ('#{name}', '#{description}', '#{price}') RETURNING id, name, description, price;")
-      Listing.new(id: result[0]['id'], name: result[0]['name'], description: result[0]['description'], price: result[0]['price'])
+    def self.add(name:, description:, price:, owner_id:)
+      result = DbConnection.query("INSERT INTO listings (name, description, price, owner_id) VALUES ('#{name}', '#{description}', '#{price}', '#{owner_id}') RETURNING id, name, description, price,owner_id;")
+      Listing.new(id: result[0]['id'], name: result[0]['name'], description: result[0]['description'], price: result[0]['price'], owner_id: result[0]['owner_id'])
     end
   end
