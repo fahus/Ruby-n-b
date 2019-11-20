a9db8dcda57a7548813456cac3350f580c1072b3
liatmoss <liatmoss@hotmail.com>
2019-11-20 10:10:04 +0000

    updated information on more_detail.erb

--- spec/feature/more_detail_spec.rb
+++ spec/feature/more_detail_spec.rb
@@ -5,4 +5,11 @@ feature 'it shows specific listings in more detail' do
     click_button 'Click here for information on Hotel California'
     expect(page).to have_content "Hotel California"
   end
+
+  scenario 'shows Hotel California in more detail' do
+    create_listing
+    visit ('/viewlistings')
+    click_button 'Click here for information on Holiday Inn'
+    expect(page).to have_content "Holiday Inn"
+  end
 end
