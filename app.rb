93282534ee6cdcc518ec25d611a730ec44682857
liatmoss <liatmoss@hotmail.com>
2019-11-21 15:37:03 +0000

    updated tests for date selection

--- app.rb
+++ app.rb
@@ -1,53 +1,59 @@
 require 'sinatra/base'
-require 'date'
 require_relative './lib/listing'
 require_relative './lib/user'
 
 class Rubynb < Sinatra::Base
   enable :sessions
 
-get '/' do
-  erb :signup
-end
+  get '/' do
+    erb :signup
+  end
 
-post '/signup' do
-  @user = User.add(email: params['email'], password: params['password'])
-  session[:email] = @user.email
-  session[:user_id] = @user.id
-  redirect '/confirmation'
-end
+  post '/signup' do
+    @user = User.add(email: params['email'], password: params['password'])
+    session[:email] = @user.email
+    session[:user_id] = @user.id
+    redirect '/confirmation'
+  end
 
-get '/confirmation' do
-  @email = session[:email]
-  erb:'confirmation'
-end
+  get '/confirmation' do
+    @email = session[:email]
+    erb:'confirmation'
+  end
 
-get '/viewlistings' do
-  @listings = Listing.all
-  erb:'listings'
-end
+  get '/viewlistings' do
+    @listings = Listing.all
+    erb:'listings'
+  end
 
-get '/add' do
-  erb :'add_listing'
-end
+  get '/add' do
+    erb :'add_listing'
+  end
 
-post '/addlistings' do
-  Listing.add(name: params['name'], description: params['description'], price: params['price'], start_date: params['start_date'], end_date: params['end_date'], owner_id: session[:user_id])
-  redirect '/viewlistings'
-end
+  post '/addlistings' do
+    Listing.add(name: params['name'], description: params['description'], price: params['price'], start_date: params['start_date'], end_date: params['end_date'], owner_id: session[:user_id])
+    redirect '/viewlistings'
+  end
 
-get '/listing/:id' do
-  @listings = Listing.all
-  @object = @listings.select do |listing|
-    listing.id == params[:id]
-  end.first
+  get '/listing/:id' do
+    @listings = Listing.all
+    @object = @listings.select do |listing|
+      listing.id == params[:id]
+    end.first
 
-  @user = User.where(id: @object.owner_id)
-  erb:'more_detail'
-end
+    @user = User.where(id: @object.owner_id)
+    erb:'more_detail'
+  end
 
+  post '/listing/select/:id' do
+    @date = params[:date]
+    @listings = Listing.all
+    @object = @listings.select do |listing|
+      listing.id == params[:id]
+    end.first
+    @user = User.where(id: @object.owner_id)
+    erb:'select_date'
+  end
 
-
-
-run! if app_file == $0
+  run! if app_file == $0
 end
